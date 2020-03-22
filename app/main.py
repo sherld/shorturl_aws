ea87c0555698a185359e68d1248c330637bdcae0
wxc <sherld@163.com>
2020-03-22 17:12:16 +0800

    Refine port to 8080

--- app/main.py
+++ app/main.py
@@ -47,4 +47,4 @@ def create_shorturl(request: schemas.ShorturlRequest, db: Session = Depends(get_
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
