a571d6bb9a91a8aa33df0d9be4cc209bae2250a3
wxc <sherld@163.com>
2020-03-22 23:28:41 +0800

    test

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
 import uvicorn
 from app import main
 
+
+class application:
+    @staticmethod
+    def run():
+        uvicorn.run(main.app, host="0.0.0.0", port=8000)
+
+
 if __name__ == '__main__':
-    uvicorn.run(main.app, host="0.0.0.0", port=8000)
+    application.run()
